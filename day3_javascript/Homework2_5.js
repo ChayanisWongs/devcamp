eeeb40bdd222e8d4d23c79a947bc994ec00d6696
chayanis <chayanis.wongs@gmail.com>
2022-04-19 23:14:12 +0700

    homework 2.5

---
+++ day3_javascript/Homework2_5.js
@@ -0,0 +1,20 @@
+function draw(n) {
+    text = '' ;
+    for (let i=0; i<n; i++) {        
+        for (let j=i; j<n-1; j++) {
+            text += '-' ;
+        }
+
+        for (let j=0; j<=i; j++) {
+            text += '*' ;
+        }    
+        
+        text += '\n';
+    }
+    console.log(text);
+}
+
+
+draw(2);
+//draw(3);
+//draw(4);
